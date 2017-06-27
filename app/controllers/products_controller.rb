059f92832a86e7dd04a3abc995229e1600bf69bf
李阳 <admin@admindeMacBook-Air.local>
2017-06-27 20:25:27 +0800

    products save

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,7 +7,7 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
   end
 
-    def add_to_cart
+  def add_to_cart
       @product = Product.find(params[:id])
          if !current_cart.products.include?(@product)
             current_cart.add_product_to_cart(@product)
